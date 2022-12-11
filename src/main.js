d72d71ba037b61173bfac20d6e5cc9d547f924b5
Vítor Del' Duca <vitordelduca@hotmail.com>
2022-12-11 07:08:08 -0300

    Update: Realizando pequenos ajustes

--- src/main.js
+++ src/main.js
@@ -1,6 +1,22 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import './assets/main.css';
+import { initializeApp } from "firebase/app";
 
-import './assets/main.css'
+const firebaseConfig = {
+  apiKey: "AIzaSyB3xhrv1RT-5fUYzxnNP4bX7MhE52N2t0Q",
+  authDomain: "desafio-emiolo.firebaseapp.com",
+  projectId: "desafio-emiolo",
+  storageBucket: "desafio-emiolo.appspot.com",
+  messagingSenderId: "1063087125828",
+  appId: "1:1063087125828:web:709a9887b0f663a0725311"
+};
 
-createApp(App).mount('#app')
+initializeApp(firebaseConfig);
+
+const app = createApp(App);
+
+app.use(router);
+
+app.mount('#app')
---
+++ src/views/ListUsers.vue
@@ -0,0 +1,1 @@
+<template>List Users</template>
