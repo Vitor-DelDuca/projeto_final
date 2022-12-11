7d36c5b37d2268aa71313504a1f7c4c3b5e50d0c
Vítor Del' Duca <vitordelduca@hotmail.com>
2022-12-11 07:35:39 -0300

    Create: Criando funcionalidades na página.

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,1 +1,54 @@
-<template>Login</template>
+<template>
+    <div class="register">
+        <h1>Fazer login com uma conta</h1>
+        <p><input type="text" placeholder="Email" v-model="email" /></p>
+        <p><input type="password" placeholder="Password" v-model="password" /></p>
+        <p v-if="errMsg">{{ errMsg }}</p>
+        <p><button @click="register">Enviar</button></p>
+        <p><button @click="signInWithGoogle">Login com Google</button></p>
+    </div>
+</template>
+
+<script setup>
+
+    import { ref } from "vue";
+    import { getAuth, signUserWithEmailAndPassword } from "firebase/auth";
+    import { useRouter } from 'vue-router'
+
+    const email = ref("");
+    const password = ref("");
+    const errMsg = ref()
+    const router = useRouter()
+
+    const register = () => {
+        const auth = getAuth()
+        signUserWithEmailAndPassword(auth, email.value, password.value)
+        .then((data) => {
+            console.log("Efetuado login com sucesso!");
+            console.log(auth.currentUser);
+            router.push('/feed')
+        })
+        .catch((error) => {
+            console.log(error.code);
+            switch (error.code) {
+                case "auth/invalid-email":
+                    errMsg.value = "E-mail invalido";
+                    break;
+                case "auth/user-not-found":
+                    errMsg.value = "Nenhuma conta com esse e-mail foi encontrada";
+                    break;
+                case "auth/wrong-password":
+                    errMsg.value = "Senha incorreta";
+                    break;
+                default:
+                    errMsg.value = "E-mail ou senha incorreto";
+                    break;
+            }
+        });
+    };
+
+    const signInWithGoogle = () => {
+
+    };
+
+</script>
