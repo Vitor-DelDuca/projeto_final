616d1507e1a9f2dd1aac58588aaebbd56ebff313
Vítor Del' Duca <vitordelduca@hotmail.com>
2022-12-11 15:23:44 -0300

    Update: Botão deslogar e funcionalidades

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,3 @@
-<script setup>
-
-</script>
-
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
@@ -10,9 +6,39 @@
     <router-link to="/feed">Feed</router-link> |
     <router-link to="/register">Register</router-link> |
     <router-link to="/login">Login</router-link> |
+    <button @click="handleSignOut" v-if="isLoggedIn" >Deslogar</button>
   </nav>
   <router-view />
 </template>
+
+<script setup>
+
+  import { onMounted, ref } from "vue";
+  import { getAuth, onAuthStateChanged, signOut } from "firebase/auth";
+  import { useRouter } from "vue-router";
+
+  const router = useRouter();
+  const isLoggedIn = ref(false);
+
+  let auth;
+  onMounted(() => {
+    auth = getAuth();
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        isLoggedIn.value = true;
+      } else {
+        isLoggedIn.value = false;
+      }
+    });
+  });
+
+  const handleSignOut = () => {
+    signOut(auth).then(() => {
+      router.push("/");
+    });
+  };
+
+</script>
 
 <style scoped>
 header {
